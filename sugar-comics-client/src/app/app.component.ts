2e591cb72dc388db67ce29821a99d084b40c5e51
CharmedLesh <tur.full.5932@gmail.com>
2023-07-18 03:53:16 +0300

    favico + hamburger menu

--- sugar-comics-client/src/app/app.component.ts
+++ sugar-comics-client/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'sugar-comics-client';
+    isHamburgerMenuOpen: boolean = false;
+
+    hamburgerMenuClick() {
+        this.isHamburgerMenuOpen = !this.isHamburgerMenuOpen;
+    }
 }
