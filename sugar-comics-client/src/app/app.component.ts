b2fc107b969dd0511c75880dec175be44fb6a0de
CharmedLesh <tur.lesh.5932@gmail.com>
2023-07-25 04:13:14 +0300

    auth modal open/close

--- sugar-comics-client/src/app/app.component.ts
+++ sugar-comics-client/src/app/app.component.ts
@@ -8,7 +8,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
     isHamburgerMenuOpen: boolean = false;
 
+    isSignInModalOpen: boolean = false;
+    isSignUpModalOpen: boolean = false;
+
     hamburgerMenuClick() {
         this.isHamburgerMenuOpen = !this.isHamburgerMenuOpen;
+    }
+
+    signInModalOpen() {
+        this.isSignInModalOpen = !this.isSignInModalOpen;
+    }
+
+    signUpModalOpen() {
+        this.isSignUpModalOpen = !this.isSignUpModalOpen;
+    }
+
+    closeAuthModal() {
+        if (this.isSignInModalOpen) {
+            this.isSignInModalOpen = false;
+        }
+
+        if (this.isSignUpModalOpen) {
+            this.isSignUpModalOpen = false;
+        }
     }
 }
